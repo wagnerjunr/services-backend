ef587ec1ef8d203bfb88a0abea8ad4dc68c29dc1
wagnerjunr <wagneraparecidojr@gmail.com>
2025-05-30 12:44:30 -0300

    feat/web-socket-connection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,25 @@
 import { app } from "./app.js";
+import { Server } from 'socket.io';
+import { createServer } from "http";
 
+const httpServer = createServer();
+const io = new Server(httpServer,{cors:{origin:"*"}});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("message", (message) => {
+    console.log("Received message:", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+
+httpServer.listen(3008, () => {
+  console.log("Socket.io server listening on port 3008");
+})
 
 app.listen({ port: 1912 }).then(() => {
   console.log("Server Running");
